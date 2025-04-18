771cc7954cc89f83d31ac730348f3f6f896ff855
VEGA <giantwhitedeer@gmail.com>
2025-04-18 17:38:16 -0400

    flyer anim; mailbox opener; scene switcher

--- immersive-final-spring2025/Assets/Scenes/sceneOne/scripts/SceneOneToTwo.cs
+++ immersive-final-spring2025/Assets/Scenes/sceneOne/scripts/SceneOneToTwo.cs
@@ -5,23 +5,74 @@ using UnityEngine.SceneManagement;
 
 public class SceneOneToTwo : MonoBehaviour
 {
+    public GameObject openButton;
+    public Animator flyerAnim;
+    public GameObject flyerObj;
+
+    public GameObject marketButton;
+    public GameObject noMarketButton;
+
+    public bool buttonOverlap = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        openButton.SetActive(false);
+        flyerObj.SetActive(false);
+
+        marketButton.SetActive(false);
+        noMarketButton.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (buttonOverlap == true)
+        {
+            openButton.SetActive(false);
+        }
     }
 
     public void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.CompareTag("sceneSwitch"))
+        if (col.gameObject.CompareTag("openMailbox"))
         {
-            SceneManager.LoadScene("Scene2");
+            openButton.SetActive(true);
         }
+    }
+
+    public void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.CompareTag("openMailbox"))
+        {
+            openButton.SetActive(false);
+        }
+    }
+
+    public void OpenMailbox()
+    {
+        flyerObj.SetActive(true);
+        flyerAnim.SetBool("flyerAnimPlay", true);
+
+        openButton.SetActive(false);
+        marketButton.SetActive(true);
+        noMarketButton.SetActive(true);
+
+        buttonOverlap = true;
+    }
+
+    public void GoToMarket()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+
+    public void NoGoToMarket()
+    {
+        flyerObj.SetActive(false);
+        marketButton.SetActive(false);
+        noMarketButton.SetActive(false);
+
+        buttonOverlap = false;
     }
 }
